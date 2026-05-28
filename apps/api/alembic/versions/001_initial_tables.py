a52f12aeb7b185fdc8c2857dd84db50b89d2656e
meganta <meganta@hotmail.com>
2026-05-28 13:20:26 +0000

    fix: sort imports in alembic migration file

--- apps/api/alembic/versions/001_initial_tables.py
+++ apps/api/alembic/versions/001_initial_tables.py
@@ -5,8 +5,8 @@ Revises:
 Create Date: 2025-01-01 00:00:00.000000
 """
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects.postgresql import UUID
 
 revision = '001'
