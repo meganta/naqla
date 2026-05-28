b386d871cdcf9cb07e4ca80cfd7f123a5f670c52
meganta <meganta@hotmail.com>
2026-05-28 01:21:59 +0000

    fix: remove unused CORSMiddleware import from worker

--- services/worker/main.py
+++ services/worker/main.py
@@ -1,7 +1,6 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
 
